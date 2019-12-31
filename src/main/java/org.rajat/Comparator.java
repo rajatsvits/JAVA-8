5c44dae11e9de14203c320c2fef3fc9d81a45c75
IN22914337 <rajat.mahajan@tesco.com>
2019-12-31 11:57:34 +0530

    imagers

--- src/main/java/org.rajat/Comparator.java
+++ src/main/java/org.rajat/Comparator.java
@@ -14,13 +14,26 @@ public interface Comparator<T> {
 //    return (p1,p2) -> f.apply(p2)-f.apply(p1);
 //  }
 
-  static Comparator<Person> comparing(Function<Person, Comparable> f) {
-    return (p1,p2) -> f.apply(p1).compareTo(f.apply(p2));
-  }
-
 //  static Comparator<Person> comparing(Function<Person, String> f) {
 //    return (p1,p2) -> f.apply(p2)-f.apply(p1);
 //  }
 
+//  static Comparator<Person> comparing(Function<Person, Comparable> f) {
+//    return (p1,p2) -> f.apply(p1).compareTo(f.apply(p2));
+//  }
+
+//  Further parametrise Person too
+  static <U >Comparator<U> comparing(Function<U, Comparable> f) {
+    return (p1,p2) -> f.apply(p1).compareTo(f.apply(p2));
+  }
+
   public int compare(T t1 , T T2);
+
+  public default Comparator<T> thenComparing(Comparator<T> cmp){
+    return (p1,p2)->compare(p1,p1)==0?this.compare(p1,p2):compare(p1,p2);
+  }
+
+  public default Comparator<T> thenComparing(Function<T, Comparable> f){
+    return thenComparing(comparing(f));
+  }
 }
--- src/main/java/org.rajat/MainComparator.java
+++ src/main/java/org.rajat/MainComparator.java
@@ -11,16 +11,19 @@ public class MainComparator {
     Comparator<Person> cmpLastName = (p1,p2) -> p1.getLastName().compareTo(p2.getLastName());
 
 //    Instead of writing as above we use this function define below
-    Function<Person,Integer> f1 = p -> p.getAge();
-    Function<Person,String> f2 = p -> p.getFirstName();
-    Function<Person,String> f3 = p -> p.getLastName();
+    Function<Person, Integer> f1 = p -> p.getAge();
+    Function<Person, String> f2 = p -> p.getFirstName();
+    Function<Person, String> f3 = p -> p.getLastName();
 
 //    To call above function we have create a comparing method.
 //    Comparator<Person> cmpPerson = Comparator.comparing(f1);
 //    its same
-    Comparator<Person> cmpPerson1 = Comparator.comparing(p->p.getAge());
+    Comparator<Person> cmpPerson1 = Comparator.comparing(p -> p.getAge());
     Comparator<Person> cmpPersonAge = Comparator.comparing(Person::getAge);
     Comparator<Person> cmpPersonFirstName = Comparator.comparing(Person::getFirstName);
+
+    Comparator<Person> cmp = Comparator.comparing(Person::getLastName)
+        .thenComparing(Person::getFirstName).thenComparing(Person::getAge);
 
   }
 }
